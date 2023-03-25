2b6946e6a905f3dca2eae197778371e65dbcac27
yuu <46545607+mouseos@users.noreply.github.com>
2023-03-25 21:44:23 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,18 +3,9 @@ sudo apt-get install build-essential acpica-tools nasm uuid-dev gcc-aarch64-linu
 export WORKSPACE=~/workspace
 mkdir $WORKSPACE
 cd $WORKSPACE
-git clone --recursive https://github.com/tianocore/edk2.git
-pushd edk2
-git checkout 46f4c9677c615d862649459392f8f55b3e6567c2
-popd
-git clone https://github.com/tianocore/edk2-non-osi.git
-pushd edk2-non-osi
-git checkout 1e2ca640be54d7a4d5d804c4f33894d099432de3
-popd
-git clone https://github.com/tianocore/edk2-platforms.git
-pushd edk2-platforms
-git checkout 861c200cda1417539d46fe3b1eba2b582fa72cbb
-popd
+git clone --recursive -o 46f4c9677c615d862649459392f8f55b3e6567c2 https://github.com/tianocore/edk2.git --depth 1
+git clone --recursive -o 1e2ca640be54d7a4d5d804c4f33894d099432de3 https://github.com/tianocore/edk2-non-osi.git --depth 1
+git clone --recursive -o 861c200cda1417539d46fe3b1eba2b582fa72cbb https://github.com/tianocore/edk2-platforms.git --depth 1
 git clone https://github.com/andreiw/rk3399-edk2.git edk2-platforms/Platform/Rockchip
 export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
